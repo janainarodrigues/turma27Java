a1b588cb344de9b0ef00ad77269a673a3941ae10
janainarodrigues <janaina.rodrigues03@icloud.com>
2021-07-23 22:03:37 -0300

    Lista Spring

---
+++ Spring/hello/controller/HelloController.java
@@ -0,0 +1,16 @@
+package comhelloword.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/hello")
+
+public class HelloController {
+	@GetMapping
+	public String hello () {
+		return "Hello Generation Brasil!!! Mentalidade de persistência sempre";
+	}
+	
+}
